1e4038f08d3f686c3bd4e0a91ffe6d03be4b64ab
Olivier 3lanc <olivier3lanc@gmail.com>
2018-07-09 14:26:24 +0200

    Add basic options to scrollshow

--- libraries/scrollshow.js/scrollshow.js
+++ libraries/scrollshow.js/scrollshow.js
@@ -1,9 +1,31 @@
 (function(jQuery){
     jQuery.fn.scrollshow = function(options) {
+        //Defaults parameters
+        var g_parameters = {
+            id:                 'scrollshow',               //ID if the main Scrollshow wrapper
+            scrollRange:        2000,                       //Amount of pixels per item
+            keepActive:         true,                       //Once scrolled, letters keep active
+            firstLetterActive:  false,                      //First letter of the sentence always active
+            textEllipsis:       '...',                      //String displayed at the end of each text to scroll
+            onItemChange:       function(e){                //Callback on item change
+
+            }
+        }
+        //Check user parameters
+        if(typeof options == 'object'){
+            //Manage parameters
+            var userProperties = Object.getOwnPropertyNames(options);
+            //Replace each parameter by its new value
+            userProperties.forEach(function(property){
+                if(g_parameters.hasOwnProperty(property)){
+                    g_parameters[property] = options[property];
+                }
+            });
+        }
         //jQuery objects
         var jQ_body = jQuery('body');
         var jQ_windowHeight = jQuery(window).height();
-        var jQ_scrollshow = jQuery('#scrollshow');
+        var jQ_scrollshow = jQuery('#'+g_parameters.id);
         //Include overlay
         jQ_scrollshow.append('<div class="overlay"></div>');
         //Current item index
@@ -13,9 +35,9 @@
         //Amount of items
         var g_amountOfItems = jQ_scrollshow.children('.item').length;
         //Scroll amount for an item
-        var g_unitRange = 2000;
+        var g_itemScrollRange = g_parameters.scrollRange;
         //Apply body height
-        jQ_body.height(g_amountOfItems * g_unitRange + jQ_windowHeight);
+        jQ_body.height(g_amountOfItems * g_itemScrollRange + jQ_windowHeight);
 
         //Navigation and progress
         //Include navigation
@@ -49,7 +71,7 @@
             var currentHash = jQuery(this).attr('href');
             var currentIndex = currentHash.replace('#','');
             currentIndex = parseInt(currentIndex);
-            var scrollAmount = currentIndex * g_unitRange;
+            var scrollAmount = currentIndex * g_itemScrollRange;
             jQuery(document).scrollTop(scrollAmount);
         });
 
@@ -58,7 +80,7 @@
             //First child of the .item is the scroller
             var jQ_text = jQuery(this).children().eq(0);
             //Text content of the element to scroll
-            var text = jQ_text.text()+' ...';
+            var text = jQ_text.text()+' '+g_parameters.textEllipsis;
             var result = '';
             var zIndex = 1000;
             for(var i = 0; i < text.length; i++){
@@ -70,6 +92,10 @@
                 zIndex--;
             }
             jQ_text.html('<span class="word">'+result+'</span>');
+            //First letter options management
+            if(g_parameters.firstLetterActive){
+                jQuery(this).addClass('first-letter-active');
+            }
         });
 
         //Update management
@@ -77,13 +103,13 @@
             //Update value of the scroll top amount
             g_scrollTopRaw = jQuery(document).scrollTop();
             //If end of scroll, decrease g_scrollTopRaw to avoid jump
-            if(g_scrollTopRaw >= (g_amountOfItems * g_unitRange)){
-                g_scrollTopRaw = g_amountOfItems * g_unitRange - 1;
+            if(g_scrollTopRaw >= (g_amountOfItems * g_itemScrollRange)){
+                g_scrollTopRaw = g_amountOfItems * g_itemScrollRange - 1;
             }
             //Calculates the displayed item in relation with scroll amount
-            g_index = parseInt(g_scrollTopRaw / g_unitRange);
+            g_index = parseInt(g_scrollTopRaw / g_itemScrollRange);
             //Amount of scroll for the current item
-            var cur_relativeScroll = g_scrollTopRaw - g_unitRange * g_index;
+            var cur_relativeScroll = g_scrollTopRaw - g_itemScrollRange * g_index;
             //CSS transform property
             // var g_transform = 'translateY(calc(-50% - '+cur_relativeScroll / 100+'px))';
             //CSS text shadow property
@@ -97,41 +123,45 @@
             //How many letters into this item title
             var cur_amountOfLetters = jQ_scrollshow.find('.item:eq('+g_index+') .letter').length;
             //Amount of scroll per step
-            var cur_scrollSteps = parseInt(g_unitRange / cur_amountOfLetters);
+            var cur_scrollSteps = parseInt(g_itemScrollRange / cur_amountOfLetters);
             //Index of the letter
             var cur_letterIndex = parseInt(cur_relativeScroll / cur_scrollSteps);
 
-            //Create event active item changed
+            //Create event active item changed for the target item
             if(!jQ_activeItem.hasClass('active')){
-                jQ_scrollshow.trigger('itemChange');
+                var e_itemChange = jQuery.Event('itemChange');
+                jQ_scrollshow.trigger({
+                    type: 'itemChange',
+                    targetIndex: g_index
+                });
             }
 
             //Transformations on the active item
             jQ_activeItem.addClass('active');
 
-            //Letters management of the active item
-            //Make only the current letter index as active
-            // jQ_activeItem
-            //     .find('.letter:eq('+cur_letterIndex+')')
-            //     .addClass('active');
-            // jQ_activeItem
-            //     .find('.letter:not(:eq('+cur_letterIndex+'))')
-            //     .removeClass('active');
-
-            //Make all the previous letters indexes active
-            for (var z = 0; z < cur_amountOfLetters; z++) {
-                if(z <= cur_letterIndex){
-                    jQ_activeItem
-                        .find('.letter:eq('+z.toString()+')')
-                        .addClass('active');
-                    g_temp_letterIndex = z;
-                }else{
-                    jQ_activeItem
-                        .find('.letter:eq('+z.toString()+')')
-                        .removeClass('active');
+            if(g_parameters.keepActive){
+                //Make all the previous letters indexes active
+                for (var z = 0; z < cur_amountOfLetters; z++) {
+                    if(z <= cur_letterIndex){
+                        jQ_activeItem
+                            .find('.letter:eq('+z.toString()+')')
+                            .addClass('active');
+                    }else{
+                        jQ_activeItem
+                            .find('.letter:eq('+z.toString()+')')
+                            .removeClass('active');
+                    }
                 }
+            }else{
+                // Letters management of the active item
+                //Make only the current letter index as active
+                jQ_activeItem
+                    .find('.letter:eq('+cur_letterIndex+')')
+                    .addClass('active');
+                jQ_activeItem
+                    .find('.letter:not(:eq('+cur_letterIndex+'))')
+                    .removeClass('active');
             }
-
 
             //Transformations on non current item
             jQ_inactiveItems.removeClass('active');
@@ -147,7 +177,7 @@
                 }
             }
             //Calculates progress ratio between 0 and 1
-            var cur_progressCoef = g_scrollTopRaw / (g_amountOfItems * g_unitRange);
+            var cur_progressCoef = g_scrollTopRaw / (g_amountOfItems * g_itemScrollRange);
             var cur_currentProgressHeight = cur_progressCoef * g_progressHeight;
             if(g_index + 1 == g_amountOfItems){
                 cur_currentProgressHeight = g_progressHeight - g_navigationBulletMargin - g_navigationBulletHeight;
@@ -170,11 +200,14 @@
             //Update window height value
             jQ_windowHeight = jQuery(window).height();
             //Update body height
-            jQ_body.height(g_amountOfItems * g_unitRange + jQ_windowHeight);
+            jQ_body.height(g_amountOfItems * g_itemScrollRange + jQ_windowHeight);
         });
 
+        //Manage itemChange callback
+        //Returns the target index
         jQ_scrollshow.on('itemChange',function(e){
-
+            var targetIndex = e.targetIndex;
+            g_parameters.onItemChange(targetIndex);
         });
     };
 }(jQuery));
