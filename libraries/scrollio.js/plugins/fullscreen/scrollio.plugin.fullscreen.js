245860c1f942924fd6b574ec6e5bea1c3d88aa99
Olivier 3lanc <olivier3lanc@gmail.com>
2018-08-27 16:14:03 +0200

    First release of Fullscreen Plugin with its options

--- libraries/scrollio.js/plugins/fullscreen/scrollio.plugin.fullscreen.js
+++ libraries/scrollio.js/plugins/fullscreen/scrollio.plugin.fullscreen.js
@@ -12,19 +12,22 @@
             //The name of the plugin
             name:           'Fullscreen Plugin',
             //Description of the plugin
-            description:    'Add a contextual and parametrable button command for full screen toggle',
+            description:    'Add a configurable button command for full screen toggle',
+            //Object containing the icons svg resources
             icons: {
+                //Minimize SVG code
                 minimize:   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3"></path></svg>',
+                //Maximize SVG code
                 maximize:   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg>'
             },
-            iconsColor: 'white'
-            //Parameters of the plugin
-            // enterFullScreen: function(){
-            //     enter();
-            // },
-            // exitFullScreen: function(){
-            //     exit();
-            // }
+            //Color of icons
+            iconsColor: 'white',
+            //Position of the icons (top, bottom)
+            position: 'top',
+            //Alignment of the icons (left, center, right)
+            alignment: 'left',
+            //Space between the icons and the edge of the viewport
+            padding: '1em'
         };
         //Plugin API
         var pluginAPI = window.scrollioAPI[scrollioPlugin.id];
@@ -32,30 +35,29 @@
         pluginAPI = scrollioPlugin;
         //Wait Scrollio detects this plugin
         jQuery(document).one(scrollioPlugin.id,function(data){
-            //Now this plugin is installed
             //Enter full screen
             var enterFs = function(element) {
                 if(element === undefined){
                     element = document.documentElement;
                 }
-                if (element.requestFullscreen) {
+                if(element.requestFullscreen){
                     element.requestFullscreen();
-                } else if (element.mozRequestFullScreen) {
+                }else if(element.mozRequestFullScreen){
                     element.mozRequestFullScreen();
-                } else if (element.webkitRequestFullscreen) {
+                }else if(element.webkitRequestFullscreen){
                     element.webkitRequestFullscreen();
-                } else if (element.msRequestFullscreen) {
+                }else if(element.msRequestFullscreen){
                     element.msRequestFullscreen();
                 }
                 pluginAPI.isFullScreen = true;
             };
             //Exit full screen
             var exitFs = function() {
-                if (document.exitFullscreen) {
+                if(document.exitFullscreen){
                     document.exitFullscreen();
-                } else if (document.mozCancelFullScreen) {
+                }else if(document.mozCancelFullScreen){
                     document.mozCancelFullScreen();
-                } else if (document.webkitExitFullscreen) {
+                }else if(document.webkitExitFullscreen){
                     document.webkitExitFullscreen();
                 }
                 pluginAPI.isFullScreen = false;
@@ -97,7 +99,7 @@
                         '}'+
                     '</style>'
                 );
-                //Include the following markup into the DOM
+                //Include the markup into the DOM
                 jQuery('body').append(
                     '<button data-scrollio-plugin="'+scrollioPlugin.id+'">'+
                         '<span class="minimize">'+scrollioPlugin.icons.minimize+'</span>'+
@@ -116,23 +118,8 @@
                     }
                 });
             });
-            //Here are the available listeners for plugin
-            // jQuery(this)
-            //     .on('initForScrollio',function(){
-            //     })
-            //     .on('itemChangeForScrollio',function(data){
-            //     })
-            //     .on('firstItemForScrollio',function(){
-            //     })
-            //     .on('lastItemForScrollio',function(){
-            //     })
-            //     .on('letterChangeForScrollio',function(){
-            //     })
-            //     .on('itemEndForScrollio',function(data){
-            //     })
-            //     .on('scrollEndForScrollio',function(){
-            //     });
         });
+        //Notify Scrollio the presence of this plugin
         jQuery.Event('iAmaScrollioPlugin');
         jQuery(document).trigger({
             type: 'iAmaScrollioPlugin',
